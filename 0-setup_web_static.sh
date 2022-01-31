0bbd5a093c1493a59c1a52caf7b5d674f2a27750
nuel07 <yehanganep@gmail.com>
2022-01-31 05:49:19 -0800

    task 0 correction

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -17,7 +17,7 @@ sudo chgrp -R ubuntu /data/
 printf %s "server {
        listen 80 default_server;
        listen [::]:80 default_server;
-       add_header X-Served-By $HOSTNAME
+       add_header X-Served-By $HOSTNAME;
        root /var/www/html;
        index index.html index.htm;
 
